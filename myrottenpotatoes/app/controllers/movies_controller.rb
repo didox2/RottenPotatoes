9922600774d1a2afe7cce2c16ecd3e37e562a2a7
Ditmar <didox222@gmail.com>
2018-01-06 12:38:30 +0000

    Exercise 1-8

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -14,5 +14,32 @@ class MoviesController < ApplicationController
     @movie = Movie.new
     # default: render 'new' template
   end
+
+  def create
+  params.require(:movie)
+  permitted = params[:movie].permit(:title,:rating,:release_date)
+  @movie = Movie.create!(permitted)
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+  end
+  
+  def edit
+  @movie = Movie.find params[:id]
+  end
+  def update
+  @movie = Movie.find params[:id]
+  params.require(:movie)
+  permitted = params[:movie].permit(:title,:rating,:release_date)
+  @movie.update_attributes!(permitted)
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+  end
+
+  def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
+  end  
   
 end
